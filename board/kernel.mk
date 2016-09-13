5ca0a152c8a8403db1e89d8eddd071161ba56fb2
Alexander Postol <grief.north@gmail.com>
2016-09-13 21:44:55 +0300

    Revert "kipper: add kernel toolchain"
    This reverts commit 308adbfb9a3b1f7986384f6247e0de66f4030604.

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,2 @@
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_kipper-64_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
